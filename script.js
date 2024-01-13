711fe0f2348e19cbf73cfd4bf3a58dabc759e4d1
Nonie <137665882+noniesatan@users.noreply.github.com>
2024-01-14 08:23:08 +1100

    update v1 14Jan24

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
  }
+ var i = 0;
+            var txt = 'Front end and Application Developer.';
+            var speed = 50;
+            
+            function typeWriter() {
+              if (i < txt.length) {
+                document.getElementById("txtmove").innerHTML += txt.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+              }
+            }
+           setInterval(()=>{
+            i = 0;
+            document.getElementById("txtmove").innerHTML = "";
+  
+            // Start typing effect again
+            typeWriter();
+           },10000)
+        
